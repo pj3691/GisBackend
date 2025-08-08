b0aa5ae5817b0dfa850b5abebf159564d20d88c6
pj3691 <1447437725@qq.com>
2025-08-08 11:12:56 +0800

    Init

---
+++ main.py
@@ -0,0 +1,113 @@
+from flask import Flask, jsonify, send_from_directory, abort, request
+from skyfield.api import load, Topos, EarthSatellite, wgs84
+from tle2czml.tle2czml import tles_to_czml
+from datetime import datetime, timedelta
+import json
+import os
+import sys
+from src.modules.caculateTotops import toTopsCaculator
+
+
+# 设置当前工作目录为 exe 所在目录（打包为exe时放开下面的注释）
+# os.chdir(os.path.dirname(sys.executable))
+out_base_dir = "./src/assets/"
+tles_dir = out_base_dir + "tles"
+czml_out_dir = out_base_dir + "output/czmlOut"
+target_czml_out_path = os.path.join(czml_out_dir, "outResult.czml")
+toTopRes_out_dir = out_base_dir + "output/toTopOut"
+jsonConfig_out_path = out_base_dir + "fontendConfig.json"
+os.makedirs(czml_out_dir, exist_ok=True)
+
+czmlTimeOffset_default = 20
+altitude_degrees_default = 88
+
+app = Flask(__name__)
+
+
+@app.route("/history", methods=["GET"])
+def history():
+    try:
+        with open(jsonConfig_out_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return jsonify(
+            {
+                "data": data,
+                "status": "success",
+            }
+        )
+    except FileNotFoundError:
+        abort(404)
+
+
+@app.route("/convert", methods=["POST"])
+def convert():
+    # 解析文件
+    if "tle_file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files["tle_file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    # 接收前端传来的参数
+    convert_options = request.form.to_dict().get("convert_options")
+    if convert_options is None:
+        return jsonify({"error": "Missing convert_options"}), 400
+    params = json.loads(convert_options)
+    czmlTimeOffset = params.get("timeOffset", czmlTimeOffset_default)
+    altitude_degrees = params.get("altitudeDegrees", altitude_degrees_default)
+
+    try:
+        resJson = {"default": [], "czml": []}
+        file_content = file.read().decode("utf-8")
+        toTopsCaculatorNew = toTopsCaculator(
+            czmlTimeOffset,
+            altitude_degrees,
+            target_czml_out_path,
+            tles_dir,
+            czml_out_dir,
+            toTopRes_out_dir,
+        )
+        toTopsCaculatorNew.setTargetCzml(
+            resJson,
+            file_content,
+            # toTopsCaculatorNew.setDefaultCzml,
+        )
+
+        try:
+            with open(jsonConfig_out_path, "w", encoding="utf-8") as fw:
+                json.dump(resJson, fw, ensure_ascii=False, indent=2)
+                return jsonify(
+                    {
+                        "status": "success",
+                        "data": {"code": 200, "path": target_czml_out_path},
+                    }
+                )
+        except FileNotFoundError:
+            abort(404)
+    except Exception as e:
+        print(e)
+        return jsonify({"error": f"Failed to read file: {str(e)}"}), 400
+
+    return jsonify({"status": "ok"})
+
+
+# 代理 tles 目录下的文件
+@app.route("/tles/<path:filename>", methods=["GET"])
+def proxy_tles(filename):
+    tles_dir = os.path.abspath("./tles")
+    if not os.path.isfile(os.path.join(tles_dir, filename)):
+        abort(404)
+    return send_from_directory(tles_dir, filename)
+
+
+# 代理 output 目录下的文件
+@app.route("/output/<path:filename>", methods=["GET"])
+def proxy_output(filename):
+    output_dir = os.path.abspath("./output")
+    if not os.path.isfile(os.path.join(output_dir, filename)):
+        abort(404)
+    return send_from_directory(output_dir, filename)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
---
+++ src/modules/caculateTotops.py
@@ -0,0 +1,218 @@
+from skyfield.api import load, Topos, EarthSatellite, wgs84
+from tle2czml.tle2czml import tles_to_czml
+
+from datetime import datetime, timedelta
+from typing import Callable
+import json
+import os
+
+
+# 获取时间范围（当前时间往后 1 天）
+ts = load.timescale()
+t0 = ts.now()
+t1 = ts.utc(t0.utc_datetime() + timedelta(days=50))
+# 加载天文常数并创建观测场景
+eph = load("./src/assets/de421.bsp")
+earth = eph["earth"]
+
+
+class toTopsCaculator:
+    """_summary_"""
+
+    caculate_time_start = ts.now()
+    """计算开始时间
+    """
+    caculate_time_start = ts.utc(t0.utc_datetime() + timedelta(days=50))
+    """计算结束时间
+    """
+    czmlTimeOffset = 0
+    """czml计算时间偏移
+    """
+    altitude_degrees = 0
+    """过境最小天顶角
+    """
+    target_czml_out_path = ""
+    """要计算的czml文件输出路径
+    """
+    tles_dir = ""
+    """默认tle存放的文件夹
+    """
+    czml_out_dir = ""
+    """默认tle计算的czml存放的文件夹
+    """
+    toTopRes_out_dir = ""
+    """过境结果存放文件夹
+    """
+
+    def __init__(
+        self,
+        czmlTimeOffset: float,
+        altitude_degrees: float,
+        target_czml_out_path: str,
+        tles_dir: str,
+        czml_out_dir: str,
+        toTopRes_out_dir: str,
+    ):
+        """构造函数
+
+        Args:
+            czmlTimeOffset (float): czml计算时间偏移
+            altitude_degrees (float): 过境最小天顶角
+            target_czml_out_path (str): 要计算的czml文件输出路径
+            tles_dir (str): 默认tle存放的文件夹
+            czml_out_dir (str): 默认tle计算的czml存放的文件夹
+            toTopRes_out_dir (str): 过境结果存放文件夹
+        """
+
+        self.czmlTimeOffset = czmlTimeOffset
+        self.altitude_degrees = altitude_degrees
+        self.target_czml_out_path = target_czml_out_path
+        self.tles_dir = tles_dir
+        self.czml_out_dir = czml_out_dir
+        self.toTopRes_out_dir = toTopRes_out_dir
+
+    def setTargetCzml(
+        self, resJson, file_content: str, callback: Callable | None = None
+    ):
+        """通过前端传入的tle计算过境时间以及对应的czml
+
+        Args:
+            resJson (_type_): 输出的fontendConfig.json的内容
+            file_content (_type_): tle文件的内容
+            callback (_type_, optional): 回调函数. Defaults to None.
+        """
+
+        # 读取tle
+        tle = file_content.splitlines()
+        lines = [line for line in tle]
+        for i in range(0, len(lines), 3):
+            name = lines[i].split("\n")[0]
+            line1 = lines[i + 1].split("\n")[0]
+            line2 = lines[i + 2].split("\n")[0]
+            # 初始化卫星
+            satellite = EarthSatellite(line1, line2, name, ts)
+            # 设置观察点
+            observer = wgs84.latlon(
+                latitude_degrees=41.76502,
+                longitude_degrees=86.11528,
+                elevation_m=0,
+            )
+            # 查找过境事件
+            times, events = satellite.find_events(
+                observer, t0, t1, altitude_degrees=self.altitude_degrees
+            )
+            self.getEventsFromResult(events, times, satellite, observer)
+            eventsMap = [event for event in list(zip(times, events)) if event[1] == 1]
+
+            if len(eventsMap) > 0:
+                try:
+                    # 写入czml结果数据
+                    czml_data = tles_to_czml(
+                        file_content,
+                        start_time=datetime.fromisoformat(eventsMap[0][0].utc_iso())
+                        - timedelta(seconds=self.czmlTimeOffset),
+                    )
+                    with open(self.target_czml_out_path, "w") as f:
+                        f.write(czml_data)
+
+                    # 写入resJson
+                    resJson["czml"].append(
+                        {
+                            "tle_file": "",
+                            "czmlFile": f"/output/outResult.czml",
+                        }
+                    )
+
+                    # 回调函数
+                    if callback is not None:
+                        callback(
+                            resJson,
+                            eventsMap[0][0].utc_iso(),
+                        )
+
+                except Exception as e:
+                    print(e)
+
+    def setDefaultCzml(self, resJson, time):
+        """通过时间计算tles文件夹下tle的czml
+
+        Args:
+            resJson (_type_): _description_
+            time (_type_): _description_
+        """
+
+        for filename in os.listdir(self.tles_dir):
+            if filename.lower().endswith(".tle"):
+                # 读取tle文件内容
+                tle_path = os.path.join(self.tles_dir, filename)
+                with open(tle_path, "r") as tle_file:
+                    tle = tle_file.read()
+
+                # 设置czml文件输出路径
+                output_filename = os.path.splitext(filename)[0] + ".czml"
+                czml_output_file = os.path.join(self.czml_out_dir, output_filename)
+
+                # 写入resJson内容
+                resJson["default"].append(
+                    {
+                        "name": os.path.splitext(filename)[0],
+                        "load": False,
+                        "showLabel": False,
+                        "path": f"/output/{output_filename}",
+                    }
+                )
+
+                # 写入czml内容
+                with open(czml_output_file, "w") as f:
+                    f.write(
+                        tles_to_czml(
+                            tle,
+                            start_time=datetime.fromisoformat(time)
+                            - timedelta(seconds=self.czmlTimeOffset),
+                        )
+                    )
+
+    def getEventsFromResult(self, events, times, satellite, observer):
+        all_passes = []
+        """过境结果
+        """
+
+        # 计算过境结果
+        for i in range(0, len(events) - 2, 3):
+            if events[i] != 0 or events[i + 2] != 2:
+                continue
+            pass_data = []
+            t_start = times[i].utc_datetime()
+            t_end = times[i + 2].utc_datetime()
+            current = t_start
+            while current <= t_end:
+                t_sample = ts.utc(current)
+                difference = satellite - observer
+                topocentric = difference.at(t_sample)
+                alt, az, distance = topocentric.altaz()
+                # 计算 10 秒前仰角判断升降状态
+                alt_prev = (
+                    (satellite - observer)
+                    .at(ts.utc(current - timedelta(seconds=10)))
+                    .altaz()[0]
+                    .degrees
+                )
+                alt_now = alt.degrees
+                status = "升轨" if alt_now > alt_prev else "降轨"
+                pass_data.append(
+                    {
+                        "time": current.isoformat(),
+                        "elevation": round(alt_now, 2),
+                        "distance_km": round(distance.km, 2),
+                        "status": status,
+                    }
+                )
+                current += timedelta(seconds=5)
+            all_passes.append(pass_data)
+
+        # 写入过境结果
+        try:
+            with open(self.toTopRes_out_dir + "/res.json", "w", encoding="utf-8") as fw:
+                json.dump(all_passes, fw, ensure_ascii=False, indent=2)
+        except FileNotFoundError as e:
+            print(e)
